4464c2bb4d1c9938ce43947a98cc68b80ba9c6d3
Rafa <rafanavarroortega@gmail.com>
2026-03-13 12:04:39 +0100

    Initial Commit

---
+++ HelloXcode/HelloXcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloXcodeApp.swift
+//  HelloXcode
+//
+//  Created by Rafa Navarro Ortega on 13/3/26.
+//
+
+import SwiftUI
+
+@main
+struct HelloXcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
